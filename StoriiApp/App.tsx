e9f9ce0f8bb93dc976c16164c04b6707ca645c5b
kieupie <109791045+kieu1004@users.noreply.github.com>
2023-06-12 14:25:11 +0700

    Home Screen

--- StoriiApp/App.tsx
+++ StoriiApp/App.tsx
@@ -4,20 +4,21 @@ import {View, Text, StyleSheet, StatusBar} from 'react-native'
 import {colors} from './src/global/styles'
 import RootNavigator from './src/navigation/RootNavigator'
 import HomeHeader from './src/components/HomeHeader'
+import HomeScreen from './src/screens/HomeScreen'
 
 export default function App(){
   return (
     <View style = {styles.container}>
 
       {/* Status Bar */}
-      <StatusBar
+      {/* <StatusBar
        barStyle = "light-content"
        backgroundColor = {colors.statusbar}
-      />
+      /> */}
 
       {/* <RootNavigator/> */}
+      <HomeScreen/>
 
-      <HomeHeader/>
     </View>
   )
 }
--- StoriiApp/src/screens/HomeScreen.js
+++ StoriiApp/src/screens/HomeScreen.js
@@ -1,47 +1,136 @@
-import React , {useState}from "react";
+import React, { useState } from "react";
 
-import{View, Text, StyleSheet, TouchableOpacity} from 'react-native'
-import {Icon} from 'react-native-elements'
-import {colors, parameters } from '../global/styles'
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
+import { Icon } from 'react-native-elements'
+import { colors, parameters } from '../global/styles'
 import HomeHeader from "../components/HomeHeader";
 
-export default function HomeScreen(){
+export default function HomeScreen() {
 
     const [delivery, setDelivery] = useState(true)
     return (
         <View style={styles.container}>
-            <HomeHeader/>
+            <HomeHeader />
 
-            <View style={{marginTop:10, flexDirection:"row", justifyContent:"space-evenly"}}>
-                <TouchableOpacity>
-                    <View style={{...styles.deliveryButton, backgroundColor:delivery?colors.primary_bold:colors.primary_light}}>
-                        <Text style={styles.deliveryText}>Delivery</Text>
-                    </View>
-                </TouchableOpacity>
+            <ScrollView
+                stickyHeaderIndices={[0]}
+                showsVerticalScrollIndicator={true}>
 
-                <TouchableOpacity>
-                    <View style={{...styles.deliveryButton, backgroundColor:delivery?colors.primary_normal:colors.primary_light}}>
-                        <Text style={styles.deliveryText}>Pick Up</Text>
+                <View style={{ marginTop: 10, flexDirection: "row", justifyContent:"space-evenly"}}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setDelivery(true)
+                        }}>
+                        <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.primary_bold : colors.primary_light }}>
+                            <Text style={styles.deliveryText}>Delivery</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={() => {
+                            setDelivery(false)
+                        }}>
+                        <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.primary_normal : colors.primary_light }}>
+                            <Text style={styles.deliveryText}>Pick Up</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.filterView}>
+                    <View style={styles.addressView}>
+                        <View style={{ flexDirection: "row", alignItems: "center", paddingLeft: 10 }}>
+                            <Icon
+                                type="material-community"
+                                name="map-marker"
+                                color={colors.primary_bold}
+                                size={26}
+                            />
+
+                            <Text style={{ marginLeft: 5 }}>86 Au Co</Text>
+                        </View>
+
+                        <View style={styles.clockView}>
+                            <Icon
+                                type="material-community"
+                                name="clock-time-four"
+                                color={colors.primary_bold}
+                                size={26}
+                            />
+
+                            <Text style={{ marginLeft: 5 }}>Now</Text>
+                        </View>
                     </View>
-                </TouchableOpacity>
-            </View>
+
+                    <View>
+                        <Icon
+                            type="material-community"
+                            name="tune"
+                            color={colors.primary_bold}
+                            size={26}
+                        />
+                    </View>
+
+                </View>
+                <View style={styles.headerTextView}>
+                    <Text style={styles.headerText}>Categories</Text>
+                </View>
+            </ScrollView>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container :{
-        flex:1
+    container: {
+        flex: 1
     },
 
-    deliveryButton :{
-        paddingHorizontal:20,
-        borderRadius:15,
-        paddingVertical:5
+    deliveryButton: {
+        paddingHorizontal: 20,
+        borderRadius: 15,
+        paddingVertical: 5
     },
 
-    deliveryText :{
-        marginLeft:5,
-        fontSize:16,
+    deliveryText: {
+        marginLeft: 5,
+        fontSize: 16,
+    },
+
+    filterView: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        marginHorizontal: 10,
+        marginVertical: 10
+    },
+
+    addressView: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: colors.primary_light,
+        paddingHorizontal: 20,
+        borderRadius: 15,
+        paddingVertical: 3
+    },
+
+    clockView: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginLeft: 20,
+        backgroundColor: colors.primary_light,
+        borderRadius: 15,
+        paddingHorizontal: 5,
+        marginRight: 20
+    },
+
+    headerText :{
+        color:colors.primary_bold,
+        fontSize:24,
+        fontWeight:"bold",
+        paddingLeft:10
+    },
+
+    headerTextView :{
+        backgroundColor:colors.primary_light,
+        paddingVertical:2,
     }
 })
