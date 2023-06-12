031aa82c32c997a103b63b1c368abddfbe4cf981
kieupie <109791045+kieu1004@users.noreply.github.com>
2023-06-12 21:19:01 +0700

    home screen done

---
+++ StoriiApp/src/global/Data.js
@@ -0,0 +1,141 @@
+export const filterData = [
+    { name: "Album", image: require('../assets/album_cate.png'), id: "0" },
+    { name: "Merchandise", image: require('../assets/mechandise_cate.png'), id: "1" },
+    { name: "Ticket", image: require('../assets/ticket_cate.jpg'), id: "2" },
+    { name: "Card", image: require('../assets/card_cate.jpg'), id: "3" },
+    { name: "Lightstick", image: require('../assets/lightstick_cate.jpg'), id: "4" },
+];
+
+export const providerData = [
+    {
+        providerName: "Storii",
+        farAway: "21",
+        businessAddress: " VietVision, Ho Chi Minh",
+        images: "https://storii.vn/assets/images/about-us-cover.png",
+        averageReview: 4.9,
+        numberOfReview: 272,
+        coordinates: { lat: -26.1888612, lng: 28.246325 },
+        discount: 10,
+        deliveryTimme: 15,
+        collectTime: 5,
+        foodType: "Album",
+        productData: [
+            {
+                name: "Album Truyen Ngan",
+                price: 350.000,
+                image: "https://api.storii.vn/staticfiles/9512002b3f8045209c2883ed83ffa48d.png"
+
+            },
+            {
+                name: "CD Nhung vet thuong lanh",
+                price: 100.000,
+                image: "https://api.storii.vn/staticfiles/c4274ef50231424ea143beb42f7468e7.png"
+            },
+            {
+                name: "CD Thu Phuong",
+                price: 100.000,
+                image: "https://api.storii.vn/staticfiles/5706a5c7fb844f88b98558ef4a56e22e.png"
+            }
+        ],
+        id:0
+    },
+
+    {
+        providerName: "VietVision",
+        farAway: "25",
+        businessAddress: " Storii, Ho Chi Minh",
+        images: "https://storii.vn/assets/images/about-us-cover.png",
+        averageReview: 3.0,
+        numberOfReview: 200,
+        coordinates: { lat: -26.1888612, lng: 28.246325 },
+        discount: 15,
+        deliveryTimme: 25,
+        collectTime: 75,
+        CategoryType: "Mechandise",
+        productData: [
+            {
+                name: "Te Te Bong",
+                price: 320.000,
+                image: "https://api.storii.vn/staticfiles/38517a7574cd46ca9cf63fbdbc9da847.png"
+
+            },
+            {
+                name: "Huy hieu RVN",
+                price: 300.000,
+                image: "https://api.storii.vn/staticfiles/bcdeffd7c59f449dbae0eeaba2412833.png"
+            },
+            {
+                name: "Cay Tote",
+                price: 200.000,
+                image: "https://api.storii.vn/staticfiles/7cef887ba25c48ba9833f901aa223684.png"
+            }
+        ],
+        id:1
+    },
+
+    {
+        providerName: "Storii",
+        farAway: "21",
+        businessAddress: " VietVision, Ho Chi Minh",
+        images: "https://storii.vn/assets/images/about-us-cover.png",
+        averageReview: 4.9,
+        numberOfReview: 272,
+        coordinates: { lat: -26.1888612, lng: 28.246325 },
+        discount: 10,
+        deliveryTimme: 15,
+        collectTime: 5,
+        foodType: "Album",
+        productData: [
+            {
+                name: "Album Truyen Ngan",
+                price: 350.000,
+                image: "https://api.storii.vn/staticfiles/9512002b3f8045209c2883ed83ffa48d.png"
+
+            },
+            {
+                name: "CD Nhung vet thuong lanh",
+                price: 100.000,
+                image: "https://api.storii.vn/staticfiles/c4274ef50231424ea143beb42f7468e7.png"
+            },
+            {
+                name: "CD Thu Phuong",
+                price: 100.000,
+                image: "https://api.storii.vn/staticfiles/5706a5c7fb844f88b98558ef4a56e22e.png"
+            }
+        ],
+        id:2
+    },
+
+    {
+        providerName: "VietVision",
+        farAway: "25",
+        businessAddress: " Storii, Ho Chi Minh",
+        images: "https://storii.vn/assets/images/about-us-cover.png",
+        averageReview: 3.0,
+        numberOfReview: 200,
+        coordinates: { lat: -26.1888612, lng: 28.246325 },
+        discount: 15,
+        deliveryTimme: 25,
+        collectTime: 75,
+        CategoryType: "Mechandise",
+        productData: [
+            {
+                name: "Te Te Bong",
+                price: 320.000,
+                image: "https://api.storii.vn/staticfiles/38517a7574cd46ca9cf63fbdbc9da847.png"
+
+            },
+            {
+                name: "Huy hieu RVN",
+                price: 300.000,
+                image: "https://api.storii.vn/staticfiles/bcdeffd7c59f449dbae0eeaba2412833.png"
+            },
+            {
+                name: "Cay Tote",
+                price: 200.000,
+                image: "https://api.storii.vn/staticfiles/7cef887ba25c48ba9833f901aa223684.png"
+            }
+        ],
+        id:3
+    },
+];
--- StoriiApp/src/screens/HomeScreen.js
+++ StoriiApp/src/screens/HomeScreen.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, FlatList, Pressable, Image, Dimensions } from 'react-native'
 import { Icon } from 'react-native-elements'
 import { colors, parameters } from '../global/styles'
-import HomeHeader from "../components/HomeHeader";
+import { Countdown } from 'react-native-countdown-component'
+
+import HomeHeader from "../components/HomeHeader"
+import { filterData, providerData } from '../global/Data'
+import ProductCard from "../components/ProductCard"
+
+
+
+const SCREEN_WIDTH = Dimensions.get('window').width
 
 export default function HomeScreen() {
 
     const [delivery, setDelivery] = useState(true)
+    const [indexCheck, setIndexCheck] = useState("0")
     return (
         <View style={styles.container}>
             <HomeHeader />
@@ -16,24 +25,26 @@ export default function HomeScreen() {
                 stickyHeaderIndices={[0]}
                 showsVerticalScrollIndicator={true}>
 
-                <View style={{ marginTop: 10, flexDirection: "row", justifyContent:"space-evenly"}}>
-                    <TouchableOpacity
-                        onPress={() => {
-                            setDelivery(true)
-                        }}>
-                        <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.primary_bold : colors.primary_light }}>
-                            <Text style={styles.deliveryText}>Delivery</Text>
-                        </View>
-                    </TouchableOpacity>
+                <View style={{ backgroundColor: colors.cardbackground, paddingBottom: 5 }}>
+                    <View style={{ marginTop: 10, flexDirection: "row", justifyContent: "space-evenly" }}>
+                        <TouchableOpacity
+                            onPress={() => {
+                                setDelivery(true)
+                            }}>
+                            <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.primary_bold : colors.primary_light }}>
+                                <Text style={styles.deliveryText}>Delivery</Text>
+                            </View>
+                        </TouchableOpacity>
 
-                    <TouchableOpacity
-                        onPress={() => {
-                            setDelivery(false)
-                        }}>
-                        <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.primary_normal : colors.primary_light }}>
-                            <Text style={styles.deliveryText}>Pick Up</Text>
-                        </View>
-                    </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => {
+                                setDelivery(false)
+                            }}>
+                            <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.primary_normal : colors.primary_light }}>
+                                <Text style={styles.deliveryText}>Pick Up</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
                 </View>
 
                 <View style={styles.filterView}>
@@ -71,9 +82,124 @@ export default function HomeScreen() {
                     </View>
 
                 </View>
+
                 <View style={styles.headerTextView}>
                     <Text style={styles.headerText}>Categories</Text>
                 </View>
+
+                <View>
+                    <FlatList
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        data={filterData}
+                        keyExtractor={(item) => item.id}
+                        extraData={indexCheck}
+                        renderItem={({ item, index }) => (
+                            <Pressable
+                                onPress={() => { setIndexCheck(item.id) }}
+                            >
+                                <View style={indexCheck === item.id ? { ...styles.smallCardSelected } : { ...styles.smallCard }}>
+                                    <Image
+                                        style={{ height: 60, width: 60, borderRadius: 30 }}
+                                        source={item.image}
+                                    />
+                                    <View>
+                                        <Text style={indexCheck === item.id ? { ...styles.smallCardTextSelected } : { ...styles.smallCardText }}>{item.name}</Text>
+                                    </View>
+                                </View>
+                            </Pressable>
+                        )}
+                    />
+                </View>
+
+                <View style={styles.headerTextView}>
+                    <Text style={styles.headerText}>Free Delivery Now</Text>
+                </View>
+
+                <View>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Text style={{ marginLeft: 15, fontSize: 16, marginTop: -10, marginRight: 5 }}>Options changing in</Text>
+                        {/* <Countdown
+                            until = {3600}
+                            size = {14}
+                            digitStyle = {{backgroundColor:colors.banner_sale}}
+                            digitTxtStyle = {{color:colors.cardbackground}}
+                            timeToShow = {['M', 'S']}
+                            timeLabels = {{m:'Min', s:'Sec'}}
+                        /> */}
+                    </View>
+
+                    <FlatList
+                        style={{ marginTop: 10, marginBottom: 10 }}
+                        horizontal={true}
+                        data={providerData}
+                        keyExtractor={(item, index) => index.toString()}
+                        showsHorizontalScrollIndicator={false}
+
+                        renderItem={({ item }) => (
+                            <View style={{ marginRight: 5 }}>
+                                <ProductCard
+                                    screenWidth={SCREEN_WIDTH * 0.8}
+                                    images={item.images}
+                                    providerName={item.providerName}
+                                    farAway={item.farAway}
+                                    businessAddress={item.businessAddress}
+                                    averageReview={item.averageReview}
+                                />
+                            </View>
+                        )}
+                    />
+                </View>
+
+
+                <View style={styles.headerTextView}>
+                    <Text style={styles.headerText}>Promotion available</Text>
+                </View>
+
+                <View>
+                    <FlatList
+                        style={{ marginTop: 10, marginBottom: 10 }}
+                        horizontal={true}
+                        data={providerData}
+                        keyExtractor={(item, index) => index.toString()}
+                        showsHorizontalScrollIndicator={false}
+
+                        renderItem={({ item }) => (
+                            <View style={{ marginRight: 5 }}>
+                                <ProductCard
+                                    screenWidth={SCREEN_WIDTH * 0.8}
+                                    images={item.images}
+                                    providerName={item.providerName}
+                                    farAway={item.farAway}
+                                    businessAddress={item.businessAddress}
+                                    averageReview={item.averageReview}
+                                />
+                            </View>
+                        )}
+                    />
+                </View>
+
+                <View style={styles.headerTextView}>
+                    <Text style={styles.headerText}>Provider in your area</Text>
+                </View>
+
+                <View style={{ width: SCREEN_WIDTH, paddingTop: 10 }}>
+                    {
+                        providerData.map(item => (
+                            <View key={item.id} style={{ paddingBottom: 20 }}>
+                                <ProductCard
+                                    screenWidth={SCREEN_WIDTH * 0.95}
+                                    images={item.images}
+                                    providerName={item.providerName}
+                                    farAway={item.farAway}
+                                    businessAddress={item.businessAddress}
+                                    averageReview={item.averageReview}
+                                />
+                            </View>
+                        ))
+                    }
+                </View>
+
             </ScrollView>
         </View>
     )
@@ -122,15 +248,53 @@ const styles = StyleSheet.create({
         marginRight: 20
     },
 
-    headerText :{
-        color:colors.primary_bold,
-        fontSize:24,
-        fontWeight:"bold",
-        paddingLeft:10
+    headerText: {
+        color: colors.primary_bold,
+        fontSize: 24,
+        fontWeight: "bold",
+        paddingLeft: 10
     },
 
-    headerTextView :{
-        backgroundColor:colors.primary_light,
-        paddingVertical:2,
+    headerTextView: {
+        backgroundColor: colors.primary_light,
+        paddingVertical: 2,
+    },
+
+    smallCard: {
+        borderRadius: 30,
+        backgroundColor: colors.cardbackground,
+        borderColor: colors.primary_light,
+        borderWidth: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 5,
+        width: 80,
+        margin: 10,
+        height: 100
+    },
+
+    smallCardSelected: {
+        borderRadius: 30,
+        backgroundColor: colors.primary_light,
+        borderColor: colors.primary_light,
+        borderWidth: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 5,
+        width: 80,
+        margin: 10,
+        height: 100
+    },
+
+    smallCardText: {
+        fontWeight: "400",
+        color: colors.primary_bold,
+        fontSize: 10
+    },
+
+    smallCardTextSelected: {
+        fontWeight: "bold",
+        color: colors.cardbackground,
+        fontSize: 10
     }
 })
