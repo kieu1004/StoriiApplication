c687a32617996c6ce675ca474f4da5566dabbebf
kieupie <109791045+kieu1004@users.noreply.github.com>
2023-06-23 16:39:55 +0700

    a

--- StoriiApp/android/app/src/main/java/com/storiiapp/MainApplication.java
+++ StoriiApp/android/app/src/main/java/com/storiiapp/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 import com.imagepicker.ImagePickerPackage;
+// import com.reactnativeimagecroppicker.RNImageCropPickerPackage;
 
 
 public class MainApplication extends Application implements ReactApplication {
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
 
           // Add this line
           // packages.add(new ImagePickerPackage());
+          // packages.add(new RNImageCropPickerPackage());
           return packages;
         }
 
--- StoriiApp/src/screens/adminScreen/ManageCategogy.js
+++ StoriiApp/src/screens/adminScreen/ManageCategogy.js
@@ -1,28 +1,300 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView, FlatList, Pressable, Image, Dimensions } from 'react-native'
-import { Icon } from 'react-native-elements'
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    Alert,
+    FlatList,
+    Modal,
+    StyleSheet,
+} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+import CategoryController from '../../backend/controllers/CategoryController';
+import CategoryModel from '../../backend/models/CategoryModel';
+
+const ManageCategory = () => {
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+
+    const [categoryName, setCategoryName] = useState('');
+    const [categoryImage, setCategoryImage] = useState(null);
+
+    const [modalVisible, setModalVisible] = useState(false);
+
+    useEffect(() => {
+        fetchCategorys();
+    }, []);
+
+    const fetchCategorys = async () => {
+        try {
+            const categories = await CategoryController.getCategoryList();
+            setFoods(categories);
+        } catch (error) {
+            console.error('Error fetching categories:', error);
+        }
+    };
+
+    const handleSelectImage = async () => {
+        try {
+            const image = await ImagePicker.openPicker({
+                width: 300,
+                height: 300,
+                cropping: true,
+                includeBase64: true,
+            });
+
+            setCategoryImage(`data:${image.mime};base64,${image.data}`);
+        } catch (error) {
+            console.log('ImagePicker Error:', error);
+        }
+    };
 
 
+    const handleAddCategory = async () => {
+        if (!categoryName || !categoryImage) {
+            Alert.alert('Lỗi', 'Vui lòng nhập đầy đủ thông tin');
+            return;
+        }
 
+        const newCategory = new CategoryModel(
+            null,
+            categoryName,
+            categoryImage,
+        );
 
+        try {
+            const foodId = await FoodController.addFood(newFood);
+            Alert.alert('Thành công', 'Thêm danh ngày sản phẩm thành công');
+            setCategoryName('');
+            setCategoryImage(null);
+            setModalVisible(false);
+            fetchCategorys();
+        } catch (error) {
+            Alert.alert('Lỗi', 'Thêm danh mục sản phẩm thất bại');
+        }
+    };
 
-export default function ManageCategory() {
+    const handleEditCategory = (category) => {
+        setModalVisible(true);
+        setSelectedCategory(category);
+        setCategoryName(category.name);
+        setCategoryImage(food.img);
+    };
+
+    const handleDeleteCategory = async (categoryId) => {
+        try {
+            await CategoryController.deleteCategory(category);
+            fetchCategorys();
+        } catch (error) {
+            console.error('Error deleting category:', error);
+        }
+    };
+
+    const handleSubmit = async () => {
+        if (selectedCategory) {
+            const updateCategory = {
+                ...selectedCategory,
+                name: categoryName,
+            };
+            await CategoryController.updateCategory(selectedCategory.id, updateCategory);
+        } else {
+            const newCategory = {
+                name: categoryName,
+            };
+            await CategoryController.addCategory(newCategory);
+        }
+
+        setModalVisible(false);
+        fetchCategorys();
+    };
+
+    const renderItem = ({ item }) => (
+        <View style={styles.itemContainer}>
+            <Image source={{ uri: item.img }} style={styles.categoryImage} />
+            <View style={styles.itemDetails}>
+                <Text style={styles.itemName}>{item.name}</Text>
+            </View>
+            <TouchableOpacity
+                style={styles.editButton}
+                onPress={() => handleEditCategory(item)}
+            >
+                <Text style={styles.editButtonText}>Sửa</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.deleteButton}
+                onPress={() => handleDeleteCategory(item.id)}
+            >
+                <Text style={styles.deleteButtonText}>Xóa</Text>
+            </TouchableOpacity>
+        </View>
+    );
 
     return (
         <View style={styles.container}>
-            <Text>Manage Category</Text>
+            <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}>
+                <Text style={styles.buttonText}>Thêm danh mục sản phẩm</Text>
+            </TouchableOpacity>
+
+            <FlatList
+                data={categories}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id.toString()}
+                style={styles.flatList}
+            />
+
+            <Modal visible={modalVisible} animationType="slide" transparent>
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <TouchableOpacity style={styles.imageButton} onPress={handleSelectImage}>
+                            {categoryImage ? (
+                                <Image source={{ uri: categoryImage }} style={styles.categoryImage} />
+                            ) : (
+                                <View style={styles.placeholderImage}>
+                                    <Text style={styles.placeholderText}>Chọn ảnh</Text>
+                                </View>
+                            )}
+                        </TouchableOpacity>
+                        <Text style={styles.modalTitle}>{selectedCategory ? 'Sửa danh mục sản phẩm' : 'Thêm danh mục sản phẩm'}</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Tên sản phẩm"
+                            value={categoryName}
+                            onChangeText={setCategoryName}
+                        />
+
+                        <TouchableOpacity style={styles.confirmButton} onPress={handleSubmit}>
+                            <Text style={styles.buttonText}>Xác nhận</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style={styles.cancelButton}
+                            onPress={() => setModalVisible(false)}
+                        >
+                            <Text style={styles.buttonText}>Hủy</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
         </View>
-    )
-}
-
-
-
-
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 10,
+    },
+    addButton: {
+        backgroundColor: 'green',
+        padding: 10,
+        borderRadius: 5,
         alignItems: 'center',
-        justifyContent: "center"
-    }
-})
+        marginBottom: 10,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    flatList: {
+        marginBottom: 10,
+    },
+    itemContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#f2f2f2',
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    categoryImage: {
+        width: 50,
+        height: 50,
+        resizeMode: 'cover',
+        borderRadius: 5,
+    },
+    itemDetails: {
+        flex: 1,
+        marginLeft: 10,
+    },
+    itemName: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    editButtonText: {
+        color: 'white',
+        fontSize: 14,
+        fontWeight: 'bold',
+    },
+    deleteButton: {
+        backgroundColor: 'red',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 3,
+        marginLeft: 10,
+    },
+    deleteButtonText: {
+        color: 'white',
+        fontSize: 14,
+        fontWeight: 'bold',
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 5,
+        width: '80%',
+    },
+    imageButton: {
+        alignSelf: 'center',
+        marginBottom: 10,
+    },
+    placeholderImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 5,
+        backgroundColor: '#ddd',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    placeholderText: {
+        fontSize: 16,
+        color: '#888',
+    },
+    modalTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+    },
+    confirmButton: {
+        backgroundColor: 'green',
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        marginBottom: 5,
+    },
+    cancelButton: {
+        backgroundColor: 'red',
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+});
+
+export default ManageCategory;
