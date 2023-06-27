f6a6cca4f2c61a9ddcd72f4ef6a1f7f33866996b
kieupie <109791045+kieu1004@users.noreply.github.com>
2023-06-27 13:53:45 +0700

    aa

--- StoriiApp/src/components/FoodForm.js
+++ StoriiApp/src/components/FoodForm.js
@@ -2,9 +2,20 @@ import React, { useState } from 'react';
 import { StyleSheet, View, TextInput, Text, Button } from 'react-native';
 import { withFormik } from 'formik';
 import * as yup from 'yup';
-import FoodController from '../backend/controllers/FoodController';
 import CurryImagePicker from './CurryImagePicker';
 import GridList from './GridList';
+
+const FormInput = ({ title, value, onChangeText, error }) => (
+  <View style={styles.inputContainer}>
+    <Text style={styles.inputTitle}>{title}</Text>
+    <TextInput
+      value={value}
+      style={styles.input}
+      onChangeText={onChangeText}
+    />
+    {error && <Text style={styles.errorText}>{error}</Text>}
+  </View>
+);
 
 const FoodForm = (props) => {
   const [subIngredients, setSubIngredients] = useState([]);
@@ -16,76 +27,82 @@ const FoodForm = (props) => {
   return (
     <View style={styles.container}>
       <CurryImagePicker image={props.values.imageUri} onImagePicked={setFoodImage} />
-      <TextInput
+      <FormInput
+        title="Name"
         value={props.values.name}
-        style={styles.longFormInput}
-        placeholder='Name'
         onChangeText={text => { props.setFieldValue('name', text) }}
+        error={props.touched.name && props.errors.name}
       />
-      <Text style={styles.validationText}> {props.errors.name}</Text>
-      <TextInput
+      <FormInput
+        title="Category"
         value={props.values.category}
-        style={styles.longFormInput}
-        placeholder='Category'
         onChangeText={text => { props.setFieldValue('category', text) }}
+        error={props.touched.category && props.errors.category}
       />
-      <Text style={styles.validationText}> {props.errors.category}</Text>
       <View style={styles.row}>
         <TextInput
-          style={styles.formInput}
+          style={[styles.input, styles.subInput]}
           onChangeText={text => { props.setFieldValue('subIngredient', text) }}
           placeholder='Sub-ingredient'
+          placeholderTextColor='#999999'
         />
         <Button
-          style={styles.button}
           title='Add'
           onPress={() => {
             setSubIngredients([...subIngredients, props.values.subIngredient]);
             props.setFieldValue('subIngredient', '');
           }}
+          color='#2F80ED'
         />
       </View>
       <GridList items={subIngredients} />
-      <Button title='Submit' onPress={() => props.handleSubmit()} />
+      <Button title='Submit' onPress={() => props.handleSubmit()} color='#2F80ED' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 32,
+    backgroundColor: '#F7F7F7',
+  },
+  inputContainer: {
+    marginBottom: 16,
+  },
+  inputTitle: {
+    marginBottom: 4,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333333',
+  },
+  input: {
+    width: 300,
+    height: 50,
+    color: '#333333',
+    borderColor: '#B5B4BC',
+    borderWidth: 1,
+    padding: 8,
+    borderRadius: 5,
+    backgroundColor: '#FFFFFF',
+    fontFamily: 'Arial',
+  },
   row: {
-    justifyContent: 'space-between',
-    alignSelf: 'stretch',
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 32
+    marginBottom: 32,
   },
-  container: {
-    width: 300,
-    alignSelf: 'center',
-    alignItems: 'center',
-    marginTop: 32,
+  subInput: {
+    flex: 1,
+    marginRight: 8,
   },
-  formInput: {
-    borderColor: '#B5B4BC',
-    borderWidth: 1,
-    padding: 8,
-    height: 50,
-    color: 'black',
-    width: '75%',
-    marginBottom: 16,
-    marginTop: 16
-  },
-  validationText: {
-    color: 'red'
-  },
-  longFormInput: {
-    width: '100%',
-    height: 50,
-    color: 'black',
-    borderColor: '#B5B4BC',
-    borderWidth: 1,
-    padding: 8,
-    margin: 16
+  errorText: {
+    color: 'red',
+    marginTop: 4,
+    fontSize: 12,
+    fontFamily: 'Arial',
   },
 });
 
