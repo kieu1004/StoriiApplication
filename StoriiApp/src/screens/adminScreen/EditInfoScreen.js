6f153d142e8622bef855f706cd7827ac9f7058c7
kieupie <109791045+kieu1004@users.noreply.github.com>
2023-06-29 21:53:02 +0700

    edit

--- StoriiApp/src/screens/adminScreen/EditInfoScreen.js
+++ StoriiApp/src/screens/adminScreen/EditInfoScreen.js
@@ -1,40 +1,69 @@
-import React from 'react'
-import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native'
-import { Formik } from 'formik'
-import { Icon } from 'react-native-elements'
-import { colors } from '../../global/styles'
-import { PrimaryButton } from '../../components/Button'
-import UserController from '../../backend/controllers/UserController'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import { Formik } from 'formik';
+import { Icon } from 'react-native-elements';
+import { colors } from '../../global/styles';
+import { PrimaryButton } from '../../components/Button';
+import Header from '../../components/Header';
+import UserController from '../../backend/controllers/UserController';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-const EditUserScreen = () => {
+const EditUserScreen = ({navigation}) => {
   const initialValues = {
     fullName: '',
-    dateOfBirth: '',
     phoneNumber: '',
     email: '',
     address: '',
-  }
+  };
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  const onDateChange = (event, selectedDate) => {
+    setShowDatePicker(false);
+    if (selectedDate) {
+      setSelectedDate(selectedDate);
+    }
+  };
+
+  const showDatePickerModal = () => {
+    setShowDatePicker(true);
+  };
+
+  const hideDatePickerModal = () => {
+    setShowDatePicker(false);
+  };
+
+  const formatDate = (date) => {
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+    return `${day} ${month} ${year}`;
+  };
 
   const editUser = async (values) => {
     try {
       const user = {
         fullName: values.fullName,
-        dateOfBirth: values.dateOfBirth,
+        dateOfBirth: selectedDate.toISOString(),
         phoneNumber: values.phoneNumber,
         email: values.email,
         address: values.address,
         role: 'admin',
-      }
+      };
 
-      const response = await UserController.updateUser(user)
-      console.log('User updated successfully:', response)
+      const response = await UserController.updateUser(user);
+      console.log('User updated successfully:', response);
     } catch (error) {
-      console.log('Error updating user:', error)
+      console.log('Error updating user:', error);
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
+      
+      <Header type="arrow-left" navigation={navigation} />
+
       <ScrollView keyboardShouldPersistTaps="always" style={styles.details}>
         <View style={styles.view1}>
           <Text style={styles.text1}>Admin Information</Text>
@@ -42,32 +71,30 @@ const EditUserScreen = () => {
 
         <Formik initialValues={initialValues} onSubmit={editUser}>
           {(props) => (
-            <View style={styles.view2}>
-              <View style={styles.view6}>
+            <View style={styles.formContainer}>
+              <View style={styles.inputContainer}>
+                <Text style={styles.title}>Full Name</Text>
                 <TextInput
-                  placeholder="Full Name"
-                  style={styles.input1}
+                  style={styles.input}
                   autoFocus={false}
                   onChangeText={props.handleChange('fullName')}
                   value={props.values.fullName}
                 />
               </View>
 
-              <View style={styles.view6}>
-                <TextInput
-                  placeholder="Date Of Birth"
-                  style={styles.input1}
-                  keyboardType="numeric"
-                  autoFocus={true}
-                  onChangeText={props.handleChange('dateOfBirth')}
-                  value={props.values.dateOfBirth}
-                />
+              <View style={styles.inputContainer}>
+                <Text style={styles.title}>Date of Birth</Text>
+                <TouchableOpacity style={styles.datePicker} onPress={showDatePickerModal}>
+                  <Text>
+                    {selectedDate ? formatDate(selectedDate) : 'Select your date of birth'}
+                  </Text>
+                </TouchableOpacity>
               </View>
 
-              <View style={styles.view6}>
+              <View style={styles.inputContainer}>
+                <Text style={styles.title}>Mobile Number</Text>
                 <TextInput
-                  placeholder="Mobile Number"
-                  style={styles.input1}
+                  style={styles.input}
                   keyboardType="number-pad"
                   autoFocus={true}
                   onChangeText={props.handleChange('phoneNumber')}
@@ -75,19 +102,11 @@ const EditUserScreen = () => {
                 />
               </View>
 
-              <View style={styles.view10}>
-                <View>
-                  <Icon
-                    name="person"
-                    style={styles.email}
-                    color={colors.text_fuzz3}
-                    type="material"
-                  />
-                </View>
-                <View style={styles.view11}>
+              <View style={styles.inputContainer}>
+                <Text style={styles.title}>Email</Text>
+                <View style={styles.emailInputContainer}>
                   <TextInput
-                    placeholder="Email"
-                    style={styles.input4}
+                    style={styles.emailInput}
                     autoFocus={false}
                     onChangeText={props.handleChange('email')}
                     value={props.values.email}
@@ -95,95 +114,106 @@ const EditUserScreen = () => {
                 </View>
               </View>
 
-              <View style={styles.view6}>
+              <View style={styles.inputContainer}>
+                <Text style={styles.title}>Address</Text>
                 <TextInput
-                  placeholder="Address"
-                  style={styles.input1}
+                  style={styles.input}
                   autoFocus={true}
                   onChangeText={props.handleChange('address')}
                   value={props.values.address}
                 />
               </View>
 
-              <View style={styles.view17}>
+              <View style={styles.buttonContainer}>
                 <PrimaryButton title="Update" onPress={props.handleSubmit} />
               </View>
             </View>
           )}
         </Formik>
+
+        {showDatePicker && (
+          <DateTimePicker
+            value={selectedDate}
+            mode="date"
+            display="default"
+            onChange={onDateChange}
+          />
+        )}
+
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: colors.background,
   },
   details: {
     paddingHorizontal: 20,
-    paddingBottom: '100%',
+    paddingBottom: "100%",
     backgroundColor: colors.banner_sale,
     borderTopRightRadius: 40,
     borderTopLeftRadius: 40,
   },
   view1: {
-    alignItems: 'center',
     marginTop: 20,
     marginBottom: 10,
-    paddingHorizontal: 15,
+    alignItems: "center",
   },
   text1: {
-    fontSize: 28,
-    color: colors.primary_normal,
+    fontSize: 25,
     fontWeight: 'bold',
+    color: colors.primary_normal
   },
-  view2: {
-    justifyContent: 'flex-start',
-    paddingHorizontal: 15,
+  formContainer: {
+    marginTop: 20,
   },
-  view6: {
-    flexDirection: 'row',
+  inputContainer: {
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  input: {
     borderWidth: 1,
     borderColor: colors.text_fuzz3,
     borderRadius: 12,
-    paddingLeft: 5,
-    marginTop: 20,
+    paddingLeft: 10,
     height: 48,
-  },
-  input1: {
     fontSize: 16,
   },
-  view10: {
+  datePicker: {
+    borderWidth: 1,
+    borderColor: colors.text_fuzz3,
+    borderRadius: 12,
+    paddingLeft: 10,
+    height: 48,
+    justifyContent: 'center',
+  },
+  emailInputContainer: {
     flexDirection: 'row',
     borderWidth: 1,
     borderColor: colors.text_fuzz3,
     borderRadius: 12,
-    paddingLeft: 5,
-    marginTop: 20,
+    paddingLeft: 10,
     height: 48,
+    alignItems: 'center',
   },
-  email: {
+  emailIcon: {
     fontSize: 24,
-    padding: 0,
-    marginBottom: 0,
-    marginTop: 11,
-    marginLeft: 2,
+    marginRight: 10,
   },
-  view11: {
-    marginLeft: 30,
-    maxWidth: '65%',
-  },
-  input4: {
+  emailInput: {
     fontSize: 16,
-    marginLeft: -20,
-    marginBottom: -10,
+    flex: 1,
   },
-  view17: {
-    marginVertical: 10,
+  buttonContainer: {
     marginTop: 15,
   },
-})
+});
 
-export default EditUserScreen
+export default EditUserScreen;
