27c1fb0f7fdf99734d6b23138e477717765c8dd7
kieupie <109791045+kieu1004@users.noreply.github.com>
2023-07-01 14:32:18 +0700

    search

--- StoriiApp/src/screens/SearchResultScreen.js
+++ StoriiApp/src/screens/SearchResultScreen.js
@@ -1,106 +1,75 @@
-import React, {useState, useEffect} from 'react'
-import { StyleSheet, Text, View, Dimensions, FlatList } from 'react-native'
-import FoodController from '../backend/controllers/FoodController'
-import { colors } from "../global/styles"
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Dimensions, FlatList } from 'react-native';
+import FoodController from '../backend/controllers/FoodController';
+import { colors } from '../global/styles';
+import SearchResultCard from '../components/SearchResultCard';
 
-import SearchResultCard from '../components/SearchResultCard'
-
-
-
-
-
-const SCREEN_WIDTH = Dimensions.get('window').width
-
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 const SearchResultScreen = ({ navigation, route }) => {
+  const [foodList, setFoodList] = useState([]);
+  
+  const loadFoodList = async () => {
+    try {
+      const foods = await FoodController.getFoodList();
+      setFoodList(foods);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const [foodList, setFoodList] = useState([]);
-    const loadFoodList = async () => {
-        try {
-            const foods = await FoodController.getFoodList();
-            setFoodList(foods);
-            return foods;
-        } catch (error) {
-            console.log(error);
-            throw error;
-        }
-    };
+  useEffect(() => {
+    loadFoodList();
+  }, []);
 
-    useEffect(() => {
-        const loadData = async () => {
-            try {
-                const result = await loadFoodList();
-                console.log(result); // Log the foods data
-            } catch (error) {
-                console.log(error); // Log any errors that occurred
-            }
-        };
+  const searchResults = foodList.filter((food) =>
+    food.name.toLowerCase().includes(route.params.item.toLowerCase())
+  );
 
-        loadData();
-    }, []);
-
-
-
-    
-    return (
-        <View style={styles.container}>
-
+  return (
+    <View style={styles.container}>
+      <View>
+        <FlatList
+          style={{ backgroundColor: colors.cardbackground }}
+          data={searchResults}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => (
+            <SearchResultCard
+              screenWidth={SCREEN_WIDTH}
+              name={item.name}
+              img={item.img}
+              price={item.price}
+              OnPressFoodCard={() => {
+                navigation.navigate('DetailScreen');
+              }}
+            />
+          )}
+          ListHeaderComponent={
             <View>
-                <FlatList
-
-                    style={{ backgroundColor: colors.cardbackground }}
-                    data={foodList}
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={({ item, index }) => (
-
-                        <SearchResultCard
-                            screenWidth={SCREEN_WIDTH}
-                            name={item._name}
-                            img={item._img}
-                            price={item._price}
-                            OnPressFoodCard={() => { navigation.navigate("DetailScreen") }}
-                        />
-
-                    )}
-
-
-                    ListHeaderComponent={
-                        <View>
-                            <Text style={styles.listHeader}>{foodList.length} Result for {route.params.item}</Text>
-                        </View>
-                    }
-
-
-                    showsVerticalScrollIndicator={false}
-
-                />
+              <Text style={styles.listHeader}>
+                {searchResults.length} Results for {route.params.item}
+              </Text>
             </View>
+          }
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </View>
+  );
+};
 
-        </View>
-    )
-}
-
-
-
-
-
-export default SearchResultScreen
-
-
-
-
+export default SearchResultScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 20
-    },
-
-    listHeader: {
-        color: colors.primary_bold,
-        fontSize: 20,
-        paddingHorizontal: 10,
-        paddingVertical: 10,
-        fontWeight: "bold"
-    }
-})
+  container: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  listHeader: {
+    color: colors.primary_bold,
+    fontSize: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    fontWeight: 'bold',
+  },
+});
